3373eca8002edef9839e02ed6bbf239a27d1e47f
susu4788 <susu4788@naver.com>
2025-04-24 15:14:01 +0900

    change ServiceDiscovery artifact

--- service-discovery/src/main/java/com/pocha/servicediscovery/ServiceDiscoveryApplication.java
+++ service-discovery/src/main/java/com/pocha/servicediscovery/ServiceDiscoveryApplication.java
@@ -1,4 +1,4 @@
-package com.pocha.msa;
+package com.pocha.servicediscovery;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- service-discovery/src/test/java/com/pocha/servicediscovery/ServiceDiscoveryApplicationTests.java
+++ service-discovery/src/test/java/com/pocha/servicediscovery/ServiceDiscoveryApplicationTests.java
@@ -1,4 +1,4 @@
-package com.pocha.msa;
+package com.pocha.servicediscovery;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
