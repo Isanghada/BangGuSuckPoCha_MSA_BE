b31fcc3d5ef2d4fe592a84560b2af84f6a5a50d6
susu4788 <susu4788@naver.com>
2025-04-24 15:15:11 +0900

    init config-server

---
+++ config-server/README.md
@@ -0,0 +1,10 @@
+### ◼개발 환경
+- Java17
+- SpringBoot 3.4.4
+- MySQL
+- IntelliJ
+
+### ◼의존성
+- Config Server
+- bootstrap
+- actuator
---
+++ config-server/src/test/java/com/pocha/configserver/ConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pocha.configserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
