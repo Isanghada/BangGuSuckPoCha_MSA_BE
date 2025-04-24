750917d5702ea3a1c9aaa849cdd7afece57b2e49
susu4788 <susu4788@naver.com>
2025-04-24 12:55:34 +0900

    init service-discovery

---
+++ service-discovery/src/test/java/com/pocha/msa/ServiceDiscoveryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pocha.msa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceDiscoveryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
